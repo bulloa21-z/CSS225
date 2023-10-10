7691ccac93b8d591b0fa53a90485aafa3aced371
bulloa21-z <146881536+bulloa21-z@users.noreply.github.com>
2023-10-10 14:47:46 -0500

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,9 @@
+# Bryan Ulloa
+# 10/10/23
+
+#Asks user for their name and greets them
+
+print("Enter your name: ")
+name = input() # Gets input from user
+print("Hello" + name) # "+" is concatenation in Python
+

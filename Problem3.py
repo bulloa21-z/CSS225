c076a51e28ebcfb164db83df2e3ba03106d88fa8
bulloa21-z <146881536+bulloa21-z@users.noreply.github.com>
2023-10-15 23:10:10 -0500

    Add files via upload

---
+++ Problem3.py
@@ -0,0 +1,14 @@
+# Bryan Ulloa
+# 10/11/23
+
+# The users, me, and the instructor are greeted with their names
+
+allowed_users = ("Bryan", "Robyn")
+
+# The allowed users will enter their names
+user_name = input("Enter your name: ")
+
+if user_name. lower() in allowed_users:
+    print(f"Hello, {user_name}!")
+else:
+    print("Hello, Welcome!")

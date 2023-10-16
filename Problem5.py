9749efb1fd18acc294d92c3bf2d1f610cfbde95e
bulloa21-z <146881536+bulloa21-z@users.noreply.github.com>
2023-10-15 23:11:28 -0500

    Add files via upload

---
+++ Problem5.py
@@ -0,0 +1,15 @@
+# Bryan Ulloa
+
+# 10/15/2023
+
+# The program that will compute MPG for a car
+
+# Enter the number of miles driven and gallons used
+miles_driven = float("Enter the number of miles driven: ")
+gallons_used = float(input("Enter the number of gallons used: "))
+
+# Calculate the MPG of the car
+mpg = miles_driven / gallons_used
+
+# Print the answer
+print(f"Your car's MPG is {mpg: 2f} miles per gallon")

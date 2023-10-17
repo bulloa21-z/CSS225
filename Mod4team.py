5243a05528644be9c9a59a8be1d7f0ab8ea819da
bulloa21-z <146881536+bulloa21-z@users.noreply.github.com>
2023-10-17 12:00:31 -0500

    Add files via upload

---
+++ Mod4team.py
@@ -0,0 +1,26 @@
+# Bryan Ulloa
+
+# 10/17/23
+
+print ("Choose an operation: ")
+print("1. Learn Python")
+print("2. Learn Java")
+print("3. Go swimming")
+print("4. Have dinner")
+print("5. Go to bed")
+print("0. Exit")
+
+choice = input("Enter 1 or 2 to choose the operation: ")
+
+if choice == "1":
+    print("How to learn python")
+if choice == "2":
+    print("How to learn Java")
+if choice == "3":
+     print("Pool time")
+if choice == "4":
+    print("Eating Food")
+if choice == "5":
+    print("Going to sleep")
+if choice == "0":
+    print("Logged out")
